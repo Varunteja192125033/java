b2cdab8d5f78a04487bc7ff3c2dd21a4fd717986
Varunteja192125033 <113225463+Varunteja192125033@users.noreply.github.com>
2022-09-10 10:07:47 +0530

    Add files via upload

---
+++ Reversestr.java
@@ -0,0 +1,20 @@
+ 	import java.io.*;
+import java.util.Scanner;
+  
+class Reversestr {
+    public static void main (String[] args) {
+        
+        String str= "TEMPLE", nstr="";
+        char ch;
+        
+      System.out.print("String: ");
+      System.out.println("TEMPLE");
+        
+      for (int i=0; i<str.length(); i++)
+      {
+        ch= str.charAt(i);
+        nstr= ch+nstr;
+      }
+      System.out.println("Reversed word: "+ nstr);
+    }
+}
